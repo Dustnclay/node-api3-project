b3ca8b2482d7e65acaac436a3d47ce9bc9e1e58e
dustin <dustnclaytiner@gmail.com>
2020-09-10 18:28:33 -0500

    done now!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const port = process.env.port || 5000;
 const server = require('./server')
-const express = require('express')
+const express = require('express') 
 // server.use(express.json())
 
 server.listen(port, () => {
